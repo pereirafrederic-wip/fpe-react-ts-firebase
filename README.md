2e509c6c77d8f73fc61c9c5d60b6c21266bd4af8
pereirafrederic <pereira.frederic.developpeur@gmail.com>
2019-07-09 10:07:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fpe-react-ts-firebase
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/fpe-react-ts-firebase)
---
+++ index.tsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+import './style.css';
+
+interface AppProps { }
+interface AppState {
+  name: string;
+}
+
+class App extends Component<AppProps, AppState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
